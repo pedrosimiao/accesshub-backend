394a9dc70801e5c942494208b3428eecb9d1e297
pedrosimiao <pedrolumiere@gmail.com>
2025-12-14 17:37:51 -0300

    feat: Initial commit. Configs base Django, DRF & autenticação AllAuth/dj-rest-auth.

---
+++ accesshub/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AccesshubConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "accesshub"
---
+++ accesshub/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+"""
+Precisamos configurar LOGIN_REDIRECT_URL?
+A falta de configuração ACCOUNT_EMAIL_VERIFICATION = 'none' pode estar gerando o entrave?
+
+"""
