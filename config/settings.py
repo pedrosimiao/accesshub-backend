0d7a806dda20f546cde617536c7184cc3bad4684
pedrosimiao <pedrolumiere@gmail.com>
2026-01-11 01:19:14 -0300

    feat: logs in prod

--- config/settings.py
+++ config/settings.py
@@ -281,3 +281,18 @@ else:
     EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
     DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+
+# forçar logs em produção
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
