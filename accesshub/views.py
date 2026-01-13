cb9bdce97d223e8da115ab57a2b8494e3abd7db0
pedrosimiao <pedrolumiere@gmail.com>
2026-01-13 04:51:55 -0300

    force response cookie

--- accesshub/views.py
+++ accesshub/views.py
@@ -1,10 +1,12 @@
 # accesshub/views.py
 
-from django.middleware.csrf import get_token
 from django.http import JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
+from django.middleware.csrf import get_token
 
 @ensure_csrf_cookie
 def get_csrf_token(request):
-    # forçar o Django a enviar o Set-Cookie: csrftoken
-    return JsonResponse({"detail": "CSRF cookie set"})
+    # forçar o Django a enviar o Set-Cookie: csrftoken no Header da resposta
+    response = JsonResponse({"detail": "CSRF cookie set"})
+    # enviar o token no corpo para debug
+    return response
